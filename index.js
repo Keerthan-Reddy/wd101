55753d34a77dbd42fd7acadf196169af477c6678
keerthan reddy <keerthan.9492@gmail.com>
2023-11-21 23:40:48 +0530

    addetional changes

--- index.js
+++ index.js
@@ -1,21 +1,75 @@
-let userForm = document.getElementById("user-form");
-let userEntries = [];
+function checkAgeValidity() {
+  const dobElement = document.getElementById("dob");
+  const dobValue = dobElement.value;
+  const dobDate = new Date(dobValue);
 
-window.addEventListener("load", () => {
-  const storedEntries = localStorage.getItem("user-entries");
-  if (storedEntries) {
-    userEntries = JSON.parse(storedEntries);
-    updateTable();
+  const today = new Date();
+  const month = dobDate.getMonth();
+  const day = dobDate.getDate();
+  let age = today.getFullYear() - dobDate.getFullYear();
+
+  let checkmonth = today.getMonth() < month;
+  let checkday = today.getMonth() === month && today.getDate() < day;
+
+  if (checkmonth || checkday) {
+    age--;
   }
-});
 
-const saveUserForm = (event) => {
+  const isValidAge = age > 18 && age < 55;
+
+  if (!isValidAge) {
+    dobElement.setCustomValidity("The age should be between 18 and 55 years");
+    dobElement.reportValidity();
+  } else {
+    dobElement.setCustomValidity("");
+  }
+}
+
+let userform = document.getElementById("user-form");
+
+const EntriesRetrieved = () => {
+  let entries = localStorage.getItem("user-entries");
+  if (entries) {
+    entries = JSON.parse(entries);
+  } else {
+    entries = [];
+  }
+  return entries;
+};
+
+let userentries = EntriesRetrieved();
+
+const displayEntries = () => {
+  const entries = EntriesRetrieved();
+  const tableEntries = entries
+    .map((entry) => {
+      const nameCell = `<td class='border px-4 py-2'>${entry.name}</td>`;
+      const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`;
+      const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`;
+      const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`;
+      const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndConditions}</td>`;
+      const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+      return row;
+    })
+    .join("\n");
+
+  const table = `<table class="table-auto w-full"><tr>
+     <th class="px-4 py-2">Name</th>
+     <th class="px-4 py-2">Email</th>
+     <th class="px-4 py-2">Password</th>
+     <th class="px-4 py-2">Dob</th>
+     <th class="px-4 py-2">Accepted terms?</th>
+     </tr>${tableEntries} </table>`;
+  let details = document.getElementById("user-entries");
+  details.innerHTML = table;
+};
+const saveUserform = (event) => {
   event.preventDefault();
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const dob = document.getElementById("dob").value;
-  const acceptedTermsAndconditions =
+  const acceptedTermsAndConditions =
     document.getElementById("acceptTerms").checked;
 
   const entry = {
@@ -23,33 +77,13 @@ const saveUserForm = (event) => {
     email,
     password,
     dob,
-    acceptedTermsAndconditions,
+    acceptedTermsAndConditions,
   };
-  userEntries.push(entry);
+  userentries.push(entry);
 
-  updateTable();
-  localStorage.setItem("user-entries", JSON.stringify(userEntries));
-
-  userForm.reset();
+  localStorage.setItem("user-entries", JSON.stringify(userentries));
+  displayEntries();
 };
 
-userForm.addEventListener("submit", saveUserForm);
-
-const updateTable = () => {
-  const tableBody = document.getElementById("user-entries-table-body");
-  tableBody.innerHTML = ""; // Clear existing entries
-
-  userEntries.forEach((entry) => {
-    const row = document.createElement("tr");
-    row.innerHTML = `
-    <td class="px-6 py-4 whitespace-nowrap">${entry.name}</td>
-    <td class="px-6 py-4 whitespace-nowrap">${entry.email}</td>
-    <td class="px-6 py-4 whitespace-nowrap">${entry.password}</td>
-    <td class="px-6 py-4 whitespace-nowrap">${entry.dob}</td>
-    <td class="px-6 py-4 whitespace-nowrap">${
-      entry.acceptedTermsAndconditions ? "Yes" : "No"
-    }</td>
-  `;
-    tableBody.appendChild(row);
-  });
-};
+userform.addEventListener("submit", saveUserform);
+displayEntries();
